f79a3c9698271aebcfd74a6aa796a669b8eb7ef3
Deme Gábor <gabor.deme@gmail.com>
2026-09-18 15:39:13 +0200

    Recover stale cloud Merlin conversations

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "merlin-shell-v10";
+const CACHE_NAME = "merlin-shell-v11";
 const SHARE_CACHE_NAME = "merlin-share-inbox-v1";
 const MAX_SHARED_IMAGES = 3;
 const MAX_SHARED_IMAGE_BYTES = 8 * 1024 * 1024;
